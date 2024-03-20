3444b024c6386af187ecc82efeb557adaaafeb16
lukiznr <lukyfriendly@gmail.com>
2024-03-20 22:02:30 +0700

    use uno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import UnoCSS from "unocss/astro";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lzif.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://lzif.github.io",
+  integrations: [
+    UnoCSS({
+      injectReset: "./node_modules/@unocss/reset/tailwind.css",
+    }),
+    mdx(),
+    sitemap(),
+    svelte(),
+  ],
 });
---
+++ src/components/ThemeToogle.svelte
@@ -0,0 +1,18 @@
+<script>
+  let themes = document.documentElement.attributes.getNamedItem('data-theme')?.value 
+
+  function toogledark(){
+    themes = "dark"
+    document.dispatchEvent(new CustomEvent('set-theme', { detail: "dark" }))
+  }
+  function tooglelight(){
+    themes = "light"
+    document.dispatchEvent(new CustomEvent('set-theme', { detail: "light" }))
+  }
+</script>
+
+{#if themes==="dark"}
+  <button on:click={tooglelight}>light</button>
+{:else}
+  <button on:click={toogledark}>dark</button>
+{/if}
