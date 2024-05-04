f3bbe91b62e464f834b8b8c40f46dbe0ae1d98db
lzif <lukyfriendly@gmail.com>
2024-05-04 01:49:06 +0000

    fix domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import UnoCSS from "unocss/astro";
 import svelte from "@astrojs/svelte";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lzif.github.io",
+  site: "https://luki.is-a.dev",
   integrations: [mdx(), sitemap(), svelte(), tailwind({applyBaseStyles: false,})]
 });
