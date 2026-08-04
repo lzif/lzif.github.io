5180aa8e32b5a003eb0ec141af24a967d3ae9fb9
lzif <lzif@users.noreply.github.com>
2026-08-04 19:34:20 +0800

    fix: rss feed references new site config

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,6 +1,6 @@
 export const prerender = true;
 
-import * as config from "$lib/config";
+import { site } from "$lib/config";
 import type { Post } from "$lib/types";
 
 export async function GET({ fetch }) {
@@ -12,18 +12,18 @@ export async function GET({ fetch }) {
   const xml = `
 		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 			<channel>
-				<title>${config.title}</title>
-				<description>${config.description}</description>
-				<link>${config.url}</link>
-				<atom:link href="${config.url}/rss.xml" rel="self" type="application/rss+xml"/>
+				<title>${site.name}</title>
+				<description>${site.description}</description>
+				<link>${site.url}</link>
+				<atom:link href="${site.url}/rss.xml" rel="self" type="application/rss+xml"/>
 				${posts
           .map(
             (post) => `
 						<item>
 							<title>${post.title}</title>
 							<description>${post.description}</description>
-							<link>${config.url}/${post.slug}</link>
-							<guid isPermaLink="true">${config.url}/${post.slug}</guid>
+							<link>${site.url}/${post.slug}</link>
+							<guid isPermaLink="true">${site.url}/${post.slug}</guid>
 							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
 						</item>
 					`,
